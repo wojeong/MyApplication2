d21786f6aa8dbd40efeda683eff0bc594a915720
woyon <nova0201!>
2021-03-20 14:59:53 -0400

    Initial

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //priviate Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Button buttonOne = findViewById(R.id.button);
+        buttonOne.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("Button Clicked");
+
+                Intent activity2Intent = new Intent(getApplicationContext(), MainActivity2.class);
+                startActivity(activity2Intent);
+            }
+        });    }
+
+}
